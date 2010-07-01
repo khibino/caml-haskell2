69f259ea3513ee09ebb863333d4b6cb6f02e2a2f
HIBINO Kei <hibi@utmc.or.jp>
2010-07-01 19:56:15 +0900

    Bugfix of scan_rec in hsLayout.

--- src/hsLayout.ml
+++ src/hsLayout.ml
@@ -10,17 +10,21 @@ let input_of_L lex_lzl =
       begin
         match want_lbr with
           | TK.SP_LEFT_BRACE(_) -> lazy (Cons (car, (scan_rec cdr)))
-          | not_lbr             -> lazy (Cons (car, (((TK.BLK_OPEN (if not_lbr == TK.EOF then 0 else TK.offset region)),
-                                                      region) ^: scan_rec cdr)))
+          | not_lbr
+            -> lazy (Cons (car, (((TK.BLK_OPEN (if not_lbr == TK.EOF then 0 else TK.offset region)),
+                                  region) ^: scan_rec cdr)))
       end
     | lazy (Cons ((tk0, reg0) as car, (lazy (Cons ((tk1, reg1), _)) as cdr)))
-        when TK.other_line_p reg0 reg1 ->
-      lazy (Cons (car, ((TK.BLK_OPEN (TK.offset reg1), reg1) ^: scan_rec cdr)))
-    | lzl -> lzl
+        when TK.other_line_p reg0 reg1
+          -> lazy (Cons (car, ((TK.BLK_LEVEL (TK.offset reg1), reg1) ^: scan_rec cdr)))
+
+    | lazy (Cons (car, cdr))           -> lazy (Cons (car, scan_rec cdr))
+    | lazy Nil as lzl                  -> lzl
   in
   let scan = function
     | lazy (Cons (((TK.SP_LEFT_BRACE | TK.K_MODULE), _), _)) as lzl -> scan_rec lzl
-    | lazy (Cons ((_, region), _)) as lzl -> scan_rec (cons (TK.BLK_OPEN (TK.offset region), region) lzl)
+    | lazy (Cons ((_, region), _)) as lzl
+      -> scan_rec (cons (TK.BLK_OPEN (TK.offset region), region) lzl)
     | lazy Nil as lzl -> lzl
   in
   scan lex_lzl
--- src/hsLayoutTest.ml
+++ src/hsLayoutTest.ml
@@ -1,2 +1,4 @@
 
-let do_test () = LazyList.to_list (HsLayout.input_of_L (HsUlexer.lazy_list stdin))
+let do_test chan_in = LazyList.to_list (HsLayout.input_of_L (HsUlexer.lazy_list chan_in))
+
+let do_stdin () = do_test stdin
